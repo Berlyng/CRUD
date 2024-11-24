45701cc5da97d6e62c687647db434ea93d4a0214
Berlyng Yena <berlyngyg@gmail.com>
2024-11-24 18:37:23 -0400

    Agregando proyecto

--- obj/Debug/net8.0/CRUD.api.RazorAssemblyInfo.cs
+++ obj/Debug/net8.0/CRUD.api.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CRUD.api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675bd9c9e5829b2f267bf29081dd8a2542ad969d")]
-[assembly: System.Reflection.AssemblyProductAttribute("CRUD.api")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CRUD.api")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
